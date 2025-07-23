75af850d1602f63f291534b46cfb61007788280f
MauroTuzzolino <tuzzolinomauro.prog05@gmail.com>
2025-07-23 13:40:21 +0200

    Initial commit

---
+++ src/main/java/maurotuzzolino/u6_w1_d3_compito/U6W1D3CompitoApplication.java
@@ -0,0 +1,13 @@
+package maurotuzzolino.u6_w1_d3_compito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U6W1D3CompitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U6W1D3CompitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/maurotuzzolino/u6_w1_d3_compito/U6W1D3CompitoApplicationTests.java
@@ -0,0 +1,13 @@
+package maurotuzzolino.u6_w1_d3_compito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U6W1D3CompitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
